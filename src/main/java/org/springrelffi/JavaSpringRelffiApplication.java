27ac9a7d3061b93278611361b87972288de725fa
Rel-ffi <kronbergdron@gmail.com>
2025-08-26 03:25:47 +0300

    initial commit

---
+++ src/main/java/org/springrelffi/JavaSpringRelffiApplication.java
@@ -0,0 +1,13 @@
+package org.springrelffi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRelffiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRelffiApplication.class, args);
+	}
+
+}
